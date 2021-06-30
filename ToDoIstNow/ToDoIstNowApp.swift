6fc2480a41ecf8f685d9b390d262e3bcab3fe88c
Mikhail <bskfrost3@yandex.ru>
2021-06-30 11:55:34 +0700

    Initial Commit

---
+++ ToDoIstNow/ToDoIstNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoIstNowApp.swift
+//  ToDoIstNow
+//
+//  Created by mac on 30.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct ToDoIstNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
