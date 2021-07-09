ad1e866ab9b9d085c122cf15538d959bf5b1d473
Mikhail <bskfrost3@yandex.ru>
2021-07-09 19:34:04 +0700

    Create TopBar.

--- ToDoIstNow/Sources/Scenes/View/Task/TaskView.swift
+++ ToDoIstNow/Sources/Scenes/View/Task/TaskView.swift
@@ -8,8 +8,42 @@
 import SwiftUI
 
 struct TaskView: View {
+
+    @State private var multiSelection = Set<UUID>()
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List(selection: $multiSelection) {
+                HStack {
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        Image(systemName: "circle")
+
+                    })
+                    Text("Programming")
+                }
+            }
+            .toolbar {
+                ToolbarItemGroup(placement: .navigationBarLeading) {
+
+                    Button(action: {}, label: {
+                        Image(systemName: "chevron.left.circle")
+                    })
+
+                    Text("Inbox")
+                        .bold()
+                }
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+
+                    Button(action: {}, label: {
+                        Image(systemName: "magnifyingglass")
+                    })
+
+                    Button(action: {}, label: {
+                        Image(systemName: "slider.horizontal.3")
+                    })
+                }
+            }
+        }
     }
 }
 
